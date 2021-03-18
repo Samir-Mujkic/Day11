74501e3a1d63d19679a0de6cf4857079a4e7637b
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-18 19:01:54 +0100

    zadatak1

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,15 +1,15 @@
 //Create an array here
-var myItems = [2, 4, 0, 8, 10];
+var myArr = [2, 4, 0, 8, 10];
 //End of creating an array
 function myArrayFunction(arr) {
   //Only change code below this line
-  
+  var myItems = [...arr];
 
 myItems[2] = 6;
  
 return myItems;
 //Only change code above this line
 }
-myArrayFunction("6");
-console.log(myArrayFunction("6"));
+myArrayFunction(myArr);
+console.log(myArrayFunction(myArr));
 module.exports = myArrayFunction;
