3d0b316b31e38349ba79ffb846cb0adfc209d1a4
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-18 18:49:48 +0100

    zadatak1

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -2,12 +2,13 @@
 var myItems = [2, 4, 0, 8, 10];
 //End of creating an array
 function myArrayFunction(arr) {
+  //Only change code below this line
   
 
 myItems[2] = 6;
  
 return myItems;
-
+//Only change code above this line
 }
 myArrayFunction("6");
 console.log(myArrayFunction("6"));
