32156b9a534c38709407dbe49e1c07744b688947
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-18 18:50:52 +0100

    zadatak5

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -2,12 +2,13 @@
 
 var myNestedArray = [["Toblerone", 5],["Milka",3]];
 function myNestedFunction(arr) {
+  //Only change code below this line
  
    myNestedArray = [...arr]
   
   return myNestedArray;
 
-
+//Only change code above this line
 }
 myNestedFunction(myNestedArray);
 console.log(myNestedFunction(myNestedArray));
