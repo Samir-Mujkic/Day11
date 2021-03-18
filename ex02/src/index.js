b9ddba1f19b2aee53511d2eb0902e94598d46b26
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-19 00:30:48 +0100

    zadatak

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,14 +1,14 @@
 var temps = [
-      monday = [31, 32, 19, 37],
-      tuesday = [32, 33, 18, 38],
-     wednesday = [30, 34, 10, 35],
-    thursday = [28, 30, 15, 27],
-   friday = [35, 38, 22, 30]
+       [31, 32, 19, 37],
+       [32, 33, 18, 38],
+      [30, 34, 10, 35],
+     [28, 30, 15, 27],
+    [35, 38, 22, 30]
 ];
 function myArrayFunction(arr){
     newTemps = [...arr];
    averageDayTemp = [];
-  for ( i = 0; i < 5; i++) {
+  for ( i = 0; i < arr.lenhth; i++) {
    (averageDayTemp = temps[0] / 4 + "," +
     temps[1] / 4 + "," +
     temps[2] / 4 +  "," +
@@ -27,4 +27,24 @@ myArrayFunction(averageDayTemp);
 console.log(myArrayFunction(averageDayTemp));
 
 
-module.exports = myArrayFunction;
+module.exports = myArrayFunction;function myMutation(arr) {
+  prviString = [arr];
+ if(  prviString.includes(["hey"], ["Hello"], ["qrstu"], ["Army"], ["Aarmy"], ["line"],["for"], ["neo"], ["no"], ["date"], ["Zebra"], ["Ole"])){
+  return true;
+ } else {
+   return false;
+ }
+  }
+console.log(myMutation(["hello", "hey"]));
+console.log(myMutation(["hello", "Hello"]));
+console.log(myMutation(["Zvxwvutsvqponmlkjihgfedcba", "qrstu"]));
+console.log(myMutation(["Mary", "Army"]));
+console.log(myMutation(["Mary", "Aarmy"]));
+console.log(myMutation(["Alien", "line"]));
+console.log(myMutation(["floor", "for"]));
+console.log(myMutation(["hello", "neo"]));
+console.log(myMutation(["voodoo", "no"]));
+console.log(myMutation(["ate", "date"]));
+console.log(myMutation(["Tiger", "Zebra"]));
+console.log(myMutation(["Noel", "Ole"]));
+module.exports = myMutation;
